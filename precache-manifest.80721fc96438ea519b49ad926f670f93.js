3638831a6c45ca2b2ee9facabed78e3d9b7514d2
abdulhalimmanik <abdulhalimmanik@users.noreply.github.com>
2020-05-05 20:07:24 +0600

    Updates

---
+++ precache-manifest.80721fc96438ea519b49ad926f670f93.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "/tech-store/static/media/productsBcg.160fddff.jpeg"
+  },
+  {
+    "revision": "b0f2b425197c9cb05eec",
+    "url": "/tech-store/static/css/main.8a988a78.chunk.css"
+  },
+  {
+    "revision": "b177d4b7b489e0dad5aa",
+    "url": "/tech-store/static/js/runtime~main.7dc53529.js"
+  },
+  {
+    "revision": "2b362308d9a07ef7d73f4865cbd132ab",
+    "url": "/tech-store/static/media/logo.2b362308.svg"
+  },
+  {
+    "revision": "3b35e7712411a8eacb19",
+    "url": "/tech-store/static/js/2.97f2c9ae.chunk.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "/tech-store/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/tech-store/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "b0f2b425197c9cb05eec",
+    "url": "/tech-store/static/js/main.2ca03a43.chunk.js"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/tech-store/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/tech-store/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/tech-store/static/media/storeBcg.46143c2f.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/tech-store/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "3b35e7712411a8eacb19",
+    "url": "/tech-store/static/css/2.fc5bf801.chunk.css"
+  },
+  {
+    "revision": "083dcffb8352e2164e6c03519375e552",
+    "url": "/tech-store/index.html"
+  }
+];
